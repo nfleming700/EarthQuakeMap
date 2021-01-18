9e5cccb53bd98f6f8e9f2d2ec5f75684419202c2
Nina Fleming <62937992+nfleming700@users.noreply.github.com>
2021-01-18 15:33:58 +0000

    Get a map

--- eq_map.js
+++ eq_map.js
@@ -1,15 +1,39 @@
 let mapbox_config;
+let mapimg;
 
-function setup ()
+function preload ()
 {
     console.log ( "setup" );
 
-    mapbox_config = loadJSON ( "mapbox.json" );
+    mapbox_config = loadJSON ( "mapbox.json",
+        () => mapimg = loadImage ( get_map_url () )
+    );
+
+    
 }
+
+function setup ()
+{
+    createCanvas ( 600, 600 );
+
+    image ( mapimg, 0, 0 );
+}
+
 
 function draw ()
 {
-    console.log ( mapbox_config );
     noLoop ();
 }
 
+function get_map_url ()
+{
+    let href = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/";
+
+    let gps = "-122.4241,37.78,14.25,0,60";
+
+    let size = "600x600";
+
+    let key = mapbox_config [ "api_key" ];
+
+    return `${href}${gps}/${size}?access_token=${key}`;
+}

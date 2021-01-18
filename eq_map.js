abf321ca10b313d70cbbfc9e2c73182aed7b147d
Nina Fleming <62937992+nfleming700@users.noreply.github.com>
2021-01-18 15:06:53 +0000

    Load mapbox api key

--- eq_map.js
+++ eq_map.js
@@ -1,9 +1,15 @@
+let mapbox_config;
+
 function setup ()
 {
     console.log ( "setup" );
+
+    mapbox_config = loadJSON ( "mapbox.json" );
 }
 
 function draw ()
 {
+    console.log ( mapbox_config );
+    noLoop ();
 }
 
